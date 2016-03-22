1778fd53f1b5d1d82e1659fccf0ee90c537145ff
Moshe Berman <moshefromsinai@gmail.com>
2016-03-22 10:21:28 -0400

    Added some code to explore default and colored scroll indicators.

--- CustomScrollViewIndicators/FirstViewController.swift
+++ CustomScrollViewIndicators/FirstViewController.swift
@@ -8,11 +8,22 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UITableViewController {
 
+    let styles = [
+        
+        "Black" : UIScrollViewIndicatorStyle.Black,
+        "White" : UIScrollViewIndicatorStyle.White,
+        "Default" : UIScrollViewIndicatorStyle.Default
+        
+        
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tableView.indicatorStyle = UIScrollViewIndicatorStyle.Black
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,40 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+        
+        
+        let index = indexPath.row % self.styles.count
+        let style = self.styles[self.styles.startIndex.advancedBy(index)]
+        
+        cell.textLabel?.text = style.0
+        
+        if style.1 == .White {
+            cell.backgroundColor = UIColor.blackColor()
+            cell.textLabel?.textColor = UIColor.whiteColor()
+        }
+        else {
+            cell.backgroundColor = UIColor.whiteColor()
+            cell.textLabel?.textColor = UIColor.blackColor()
+        }
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        let index = indexPath.row % self.styles.count
+        let style = self.styles[self.styles.startIndex.advancedBy(index)]
+        
+        self.tableView.indicatorStyle = style.1
+        
+        self.tableView.flashScrollIndicators()
+    }
 }
 
--- CustomScrollViewIndicators/SecondViewController.swift
+++ CustomScrollViewIndicators/SecondViewController.swift
@@ -8,18 +8,93 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class SecondViewController: UITableViewController {
+    
+    let colors = [
+        "Red" : UIColor.redColor(),
+        "Orange" : UIColor.orangeColor(),
+        "Yellow" : UIColor.yellowColor(),
+        "Green" : UIColor.greenColor(),
+        "Cyan" : UIColor.cyanColor(),
+        "Blue" : UIColor.blueColor(),
+        "Purple" : UIColor.purpleColor(),
+        "Pink" : UIColor.magentaColor()
+        ].sort { (first : (name : String, color : UIColor), second: ( name : String, color : UIColor)) -> Bool in
+        
+            var result = false
+            let desiredOrder = ["Red", "Orange", "Yellow", "Green", "Cyan", "Blue", "Purple", "Pink"]
+            
+            let firstName = first.name
+            let secondName = second.name
+            
+            if desiredOrder.indexOf(firstName) < desiredOrder.indexOf(secondName) {
+                result = true
+            }
+            
+            return result
+            
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let color = self.colors[self.colors.startIndex]
+        
+        self.setScrollIndicatorColor(color.1)
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+        
+        let index = indexPath.row % self.colors.count
+        let color = self.colors[self.colors.startIndex.advancedBy(index)]
+        
+        cell.textLabel?.text = color.0
+        cell.textLabel?.textColor = color.1
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.colors.count * 3
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        let index = indexPath.row % self.colors.count
+        let color = self.colors[self.colors.startIndex.advancedBy(index)]
+        
+        self.setScrollIndicatorColor(color.1)
+        
+    }
+    
+    // MARK: - Color the Scroll View
+    
+    func setScrollIndicatorColor(color: UIColor) {
+        
+        for view in self.tableView.subviews {
+            if view.isKindOfClass(UIImageView.self),
+                let imageView = view as? UIImageView,
+                let image = imageView.image  {
+                
+                imageView.tintColor = color
+                imageView.image = image.imageWithRenderingMode(.AlwaysTemplate)
+            }
+        }
+        
+        self.tableView.flashScrollIndicators()
+    }
 }
 
