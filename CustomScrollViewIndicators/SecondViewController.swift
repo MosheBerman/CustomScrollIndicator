f9556267e680975d7c701304776e7d03c9ee0316
Moshe Berman <moshefromsinai@gmail.com>
2016-03-22 08:22:57 -0400

    Initial Commit

---
+++ CustomScrollViewIndicators/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CustomScrollViewIndicators
+//
+//  Created by Moshe Berman on 3/22/16.
+//  Copyright © 2016 Moshe Berman. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
